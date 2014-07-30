f099f353c2a84b6d4b9d99164c27c9b3a41bff49
Lance <sakrethgarlon@gmail.com>
2014-07-29 20:25:09 -0500

    restructure and button working

--- 8ledbutton/8ledbutton.ino
+++ 8ledbutton/8ledbutton.ino
@@ -1,16 +1,24 @@
 // 8led.ino
 //LED Pin Variables
 int ledPins[] = {2,3,4,5,6,7,8,9};
+int inPin = 13;
+int button = 0;
+
 
 void setup(){
+	pinMode(inPin,INPUT);
 	for(int i = 0; i < 8; i++){
 		pinMode(ledPins[i],OUTPUT);
 	}
 }
 
 void loop() {
-KnightRider();
+	button = digitalRead(inPin);
+	if (button == HIGH){
+		KnightRider();
+	}
 }
+
 void KnightRider(){
 	for(int i = 0; i <= 7; i++){
 		int onLed = i;
